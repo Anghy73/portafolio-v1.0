f7fc1111d663e47ac79f2caf23f9aae5931ecbf1
Andy <anghy.ruz@gmail.com>
2025-06-14 19:22:16 -0400

    bgpc

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,23 @@
 function Header() {
   return (
-    <header className="fixed top-0 left-0 w-full h-20 z-10">
+    <header
+      // style={{
+      //   backgroundColor: 'transparent',
+      //   backdropFilter: "blur(0px)"
+      // }}
+      className="fixed top-0 left-0 w-full h-20 z-10 backdrop-blur-xl">
       <nav className="w-full h-full flex justify-center items-center">
-        <ul className="flex justify-center items-center gap-10">
-          <li>
+        <ul className="flex justify-center items-center gap-10 text-white text-shadow-[0px_0px_8px] text-shadow-white font-medium">
+          <li className="hover:text-teal-500 hover:text-shadow-[0px_0px_8px] hover:text-shadow-teal-600 transition-all">
             <a href="#">About me</a>
           </li>
-          <li>
+          <li className="hover:text-teal-500 hover:text-shadow-[0px_0px_8px] hover:text-shadow-teal-600">
             <a href="#">Projects</a>
           </li>
-          <li>
+          <li className="hover:text-teal-500 hover:text-shadow-[0px_0px_8px] hover:text-shadow-teal-600">
             <a href="#">Skills</a>
           </li>
-          <li>
+          <li className="hover:text-teal-500 hover:text-shadow-[0px_0px_8px] hover:text-shadow-teal-600">
             <a href="#">Contacts</a>
           </li>
         </ul>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,6 +4,7 @@ function Home() {
   return (
     <ScrollAnimation>
       <div className='bgpc relative w-full'>
+        <div className="bgpcs"></div>
         <div className='noise relative w-full h-screen flex justify-center items-center overflow-hidden pointer-events-none select-none bg-[#0a0a0a]'>
           <h1 className='text-slate-500'>Portafolio 1.0</h1>
         </div>
